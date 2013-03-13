e1b9e0fe1ea36ab756e1704a89e772e9639997fe
Josh Matthews <josh@jmatthews.us>
2013-03-13 12:18:04 -0400

    Starting to split script into functions.

--- link.sh
+++ link.sh
@@ -1,86 +1,93 @@
 #!/usr/bin/env bash
 
-# Exit if any of the commands fail.
-# set -e
-
-# This string will contain regex for any
-# files or directories you want to ignore.
-link_ignore=""
-
 # This directory will contain any custom extension scripts.
 ext_dir=".link_ext"
 
-# Some default ignores.
-link_ignore="link\.sh $ext_dir \.gitmodules \.gitignore"
+function link_dotfiles()
+{
+	# This string will contain regex for any
+	# files or directories you want to ignore.
+	link_ignore=""
 
-# Get current directory and go there.
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd $dir &> /dev/null
+	# Some default ignores.
+	link_ignore="link\.sh $ext_dir \.gitmodules \.gitignore"
 
-# Now we run any custom extensions.
-if [ -d $ext_dir ]
-then
-	for script in $(ls $ext_dir)
-	do
-		source $ext_dir/$script
-	done
-fi
+	# Get current directory and go there.
+	dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	pushd $dir &> /dev/null
 
-# Now symlink and files that git is tracking
-# but that haven't been added to the ignore array.
-echo -e "\e[1;35mSymlinking dotfiles:\e[m"
-for file in $(git ls-files)
-do
-	ignoreThis=false
-
-	for word in ${link_ignore}
-	do
-		if echo $file | grep -q $word
-		then
-			ignoreThis=true
-			break
-		fi
-	done
-
-	if [ $ignoreThis != true ]
+	# Now we run any custom extensions.
+	if [ -d $ext_dir ]
 	then
-		if [ "$(readlink ~/.$file)" != "$dir/$file" ]
-		then
-			echo $file
-			if test ! -d `dirname ~/.$file`
-			then
-				mkdir -p `dirname ~/.$file`
-			fi
-			if test -h ~/.$file
-			then
-				unlink ~/.$file
-			fi
-			rm -rf ~/.file 2>&1 >/dev/null
-			ln -sf $dir/$file ~/.$file
-		fi
+		for script in $(ls $ext_dir)
+		do
+			source $ext_dir/$script
+		done
 	fi
-done
 
-# Last, we install a post-merge hook to keep everything up to date.
-if ! [ -f $dir/.git/hooks/post-merge ]
-then
-	echo "Installing post merge hook"
-	hook="$dir/.git/hooks/post-merge"
-	echo "#!/usr/bin/env bash" > $hook
-	echo "cd $dir" >> $hook
-	echo "git submodule update --init --recursive" >> $hook
+	# Now symlink and files that git is tracking
+	# but that haven't been added to the ignore array.
+	echo -e "\e[1;35mSymlinking dotfiles:\e[m"
+	for file in $(git ls-files)
+	do
+		ignoreThis=false
 
-	# if this script was run with any arguments then we want
-	# to keep them when it's run by the hook.
-	echo "./link.sh $*" >> $hook
+		for word in ${link_ignore}
+		do
+			if echo $file | grep -q $word
+			then
+				ignoreThis=true
+				break
+			fi
+		done
 
-	# Make it executable
-	chmod 755 $hook
+		if [ $ignoreThis != true ]
+		then
+			if [ "$(readlink ~/.$file)" != "$dir/$file" ]
+			then
+				echo $file
+				if test ! -d `dirname ~/.$file`
+				then
+					mkdir -p `dirname ~/.$file`
+				fi
+				if test -h ~/.$file
+				then
+					unlink ~/.$file
+				fi
+				rm -rf ~/.file 2>&1 >/dev/null
+				ln -sf $dir/$file ~/.$file
+			fi
+		fi
+	done
 
-	# This seems to be the first run, so we'll go ahead
-	# and initialize the submodules.
-	echo "Assuming submodules are empty, initializing now"
-	git submodule update --init --recursive
-fi
+	# Last, we install a post-merge hook to keep everything up to date.
+	install_post_merge_hook
 
-popd &> /dev/null
+	popd &> /dev/null
+}
+
+function install_post_merge_hook()
+{
+	if ! [ -f $dir/.git/hooks/post-merge ]
+	then
+		echo "Installing post merge hook"
+		hook="$dir/.git/hooks/post-merge"
+		echo "#!/usr/bin/env bash" > $hook
+		echo "cd $dir" >> $hook
+		echo "git submodule update --init --recursive" >> $hook
+
+		# if this script was run with any arguments then we want
+		# to keep them when it's run by the hook.
+		echo "./link.sh $*" >> $hook
+
+		# Make it executable
+		chmod 755 $hook
+
+		# This seems to be the first run, so we'll go ahead
+		# and initialize the submodules.
+		echo "Assuming submodules are empty, initializing now"
+		git submodule update --init --recursive
+	fi
+}
+
+link_dotfiles
